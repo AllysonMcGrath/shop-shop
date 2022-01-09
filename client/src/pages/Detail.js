ab48a003dae2cd72c5094d4e1c7216e1c38ae574
Allyson McGrath <allyson.r.mcgrath@gmail.com>
2022-01-09 12:15:37 -0600

    add cart persistence

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -5,7 +5,8 @@ import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
 import Cart from '../components/Cart'
-import { useStoreContext } from "../utils/GlobalState";
+import { useStoreContext } from '../utils/GlobalState';
+import { idbPromise } from '../utils/helpers';
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -26,28 +27,48 @@ function Detail() {
   useEffect(() => {
     if (products.length) {
       setCurrentProduct(products.find(product => product._id === id));
-    } else if (data) {
+    } 
+    else if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
     }
-  }, [products, data, dispatch, id]);
+    else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
 
   const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+    const itemInCart = cart.find((cartItem) => cartItem._id === id)
 
     if (itemInCart) {
-      dispatch ({
+      dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-      })
+      });
+      // if we're updating quantity, use existing item data and increment purchaseQuantity value by one
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
     } else {
       dispatch({
         type: ADD_TO_CART,
         product: { ...currentProduct, purchaseQuantity: 1 }
       });
+      // if product isn't in the cart yet, add it to the current shopping cart in IndexedDB
+      idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
     }
   };
 
@@ -56,6 +77,8 @@ function Detail() {
       type: REMOVE_FROM_CART,
       _id: currentProduct._id
     });
+
+    idbPromise('cart', 'delete', { ...currentProduct});
   };
 
   return (
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -4,3 +4,64 @@ export function pluralize(name, count) {
   }
   return name + 's'
 }
+
+export function idbPromise(storeName, method, object) {
+  return new Promise((resolve, reject) => {
+    //open connection to database with version 1
+    const request = window.indexedDB.open('shop-shop', 1);
+
+    //create variables to hold reference to database, transaction, object store
+    let db, tx, store;
+
+    //if version changed or first time using database, run this and create object stores
+    request.onupgradeneeded = function(e) {
+      const db = request.result;
+      //create object store for each and set key to _id
+      db.createObjectStore('products', { keyPath: '_id' });
+      db.createObjectStore('categories', { keyPath: '_id' })
+      db.createObjectStore('cart', { keyPath: '_id' })
+    };
+    //handle errors
+    request.onerror = function(e) {
+      console.log('There was an error');
+    };
+
+    request.onsuccess = function(e) {
+      //save reference of database to db variable
+      db = request.result;
+      //open transaction to do what is passed into storeName
+      tx = db.transaction(storeName, 'readwrite');
+      //save reference to that object store
+      store = tx.objectStore(storeName);
+
+      //if errors let us know
+      db.onerror = function(e) {
+        console.log('error', e);
+      };
+
+      switch (method) {
+        case 'put':
+          store.put(object);
+          resolve(object);
+          break;
+        case 'get':
+          const all = store.getAll();
+          all.onsuccess = function() {
+            resolve(all.result);
+          };
+          break;
+        case 'delete':
+          store.delete(object._id);
+          break;
+        default:
+          console.log('No valid method');
+          break;
+      }
+      //when transaction complete close connection
+      tx.oncomplete = function() {
+        db.close();
+      };
+    };
+
+  });
+}
